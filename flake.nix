7b6981b7f47a043d9d6560eacfe301cc36384baa
marukun712 <marukun530@gmail.com>
2026-02-24 19:47:20 +0900

    First Commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.elixir
+            pkgs.erlang
+            pkgs.  elixir-ls
+          ];
+        };
+      }
+    );
+}
