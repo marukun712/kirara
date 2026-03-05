dc52bacb92b6b36fa81c08d747df404f506c1067
marukun712 <marukun530@gmail.com>
2026-03-05 15:06:06 +0900

    actor

--- discord/index.ts
+++ discord/index.ts
@@ -4,19 +4,25 @@ import { ActionListener } from "core/listener";
 import { CharacterSchema, type InputMessage } from "core/types";
 import { Client } from "discord.js";
 import { createActor } from "xstate";
-import { generate, id, refresh } from "./agent/writer.ts";
+import { Agent } from "./agent";
+import { createDiscordMcpServer } from "./agent/mcp/discord";
 
 const TICK = 2500;
+
+const id = "polka";
+const name = "高橋ポルカ";
 
 const client = new Client({
 	intents: ["Guilds", "GuildMessages", "MessageContent"],
 });
 client.login(process.env.DISCORD_TOKEN);
 
-async function setup() {
+const mcp = createDiscordMcpServer(client);
+
+async function setup(agent: Agent) {
 	let char = Bun.file(`./data/${id}.yml`);
 	if (!(await char.exists())) {
-		await generate();
+		await agent.generate();
 		char = Bun.file(`./data/${id}.yml`);
 	}
 	const parsed = CharacterSchema.parse(YAML.parse(await char.text()));
@@ -28,22 +34,27 @@ async function setup() {
 	return { actor, listener };
 }
 
-const { actor, listener } = await setup();
+const agent = new Agent(id, name, { discord: mcp });
+
+const { actor, listener } = await setup(agent);
 const events: InputMessage[] = [];
 
 const send = (event: string, data: unknown) => {
 	actor.send({ type: "PROCESS_EVENT", eventType: event, eventData: data });
 };
 
-actor.subscribe((snapshot) => {
+actor.subscribe(async (snapshot) => {
 	const params = snapshot.context.params;
 	console.log(params);
 	const res = listener.check(params);
 	console.log(res);
+	if (res.length !== 0) {
+		await agent.act(res.join("\n"));
+	}
 });
 
 setInterval(() => send("tick", { timestamp: Date.now() }), TICK);
-setInterval(() => refresh(events), 30 * 60 * 1000);
+setInterval(() => agent.refresh(events), 30 * 60 * 1000);
 
 client.on("messageCreate", (message) => {
 	if (!client.user) return;
@@ -52,5 +63,6 @@ client.on("messageCreate", (message) => {
 	}
 	const isMentioned = message.mentions.users.has(client.user.id);
 	const event = isMentioned ? "mention" : "msg";
+	console.log(event, isMentioned);
 	send(event, { from: message.author.displayName, content: message.content });
 });
